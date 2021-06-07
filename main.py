b0b85bd3ca2d4b5a2c7447e4c44b72867a790a20
piotrek <piotrek.belda@wp.pl>
2021-06-07 21:16:52 +0200

    lacking arg

--- main.py
+++ main.py
@@ -13,9 +13,9 @@ def main():
     configure(dev_mode=True)
     uvicorn.run(app, host='127.0.0.1', port=8000)
 
-def configure(dev_mode: bool = False):
-    configure_routes()
-    configure_templates()
+def configure(dev_mode: bool):
+    configure_routes(dev_mode)
+    configure_templates(dev_mode)
     configure_db(dev_mode)
 
 def configure_routes():
@@ -24,14 +24,14 @@ def configure_routes():
     app.include_router(account.router)
     app.include_router(packages.router)
 
-def configure_templates():
+def configure_templates(dev_mode: bool):
     fastapi_chameleon.global_init('templates')
 
-def configure_db(dev_mode: bool = False):
+def configure_db(dev_mode: bool):
     file = (Path(__file__).parent / 'db' / 'pypi.sqlite').absolute()
     db_session.global_init(file.as_posix())
 
 if __name__ == "__main__":
     main()
 else:
-    configure()
+    configure(dev_mode=False)
